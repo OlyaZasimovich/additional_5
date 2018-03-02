050261212977b4ab9b1408704a52dfe3e124130b
OlyaZasimovich <olechka-zosya@mail.ru>
2018-03-02 18:37:47 +0300

    '6'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var firstBracket = '';
+	var secondBracket = '';
+	var twoBracketsArray = [];
+	var oldStr = str;
+
+	for (var i=0; i<=bracketsConfig.length-1; i++) {
+		firstBracket = bracketsConfig[i][0];
+    secondBracket = bracketsConfig[i][1];
+    twoBracketsArray.push(firstBracket + secondBracket);
+	};
+
+  
+  for (var j=0; j<=oldStr.length / 2; j++) {
+    for (var k=0; k<twoBracketsArray.length; k++) {
+    	var replaceStr = twoBracketsArray[k];
+  		str = str.replace(replaceStr, '');
+    };
+  };
+
+  if (str.length === 0) {
+    console.log("true");
+  	return true;
+  }
+
+  console.log("false");
+  return false;
 }
